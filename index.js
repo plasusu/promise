8f201657e05812e294cd84d6faa24ea811b3d682
plasusu <zhaobin0511@qq.com>
2018-07-23 11:25:17 +0800

    第二版：增加处理异步方法能力

--- index.js
+++ index.js
@@ -4,15 +4,23 @@ class ZPromise {
         this.status = 'pending'
         this.value = undefined
         this.reason = undefined
+        this.resolveArray = []
+        this.rejectArray = []
 
         function resolve(value) {
             self.status = 'resolved'
             self.value = value
+            self.resolveArray.forEach(v => {
+                v(self.value)
+            })
         }
 
         function reject(reason) {
             self.status = 'rejected'
             self.reason = reason
+            self.rejectArray.forEach(v => {
+                v(self.reason)
+            })
         }
 
         try {
@@ -23,7 +31,10 @@ class ZPromise {
     }
 
     then(resolve, reject) {
-        if (this.status === 'resolved') {
+        if (this.status === 'pending') {
+            this.resolveArray.push(resolve)
+            this.rejectArray.push(reject)
+        } else if (this.status === 'resolved') {
             resolve(this.value)
         } else if (this.status = 'rejected') {
             reject(this.reason)
@@ -31,9 +42,18 @@ class ZPromise {
     }
 }
 
-
+// v1: 只能处理同步方法
 new ZPromise((resolve, reject) => {
     resolve('success')
+}).then((val) => {
+    console.log(val)
+})
+
+// v2: 处理异步能力
+new ZPromise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('success')
+    })
 }).then((val) => {
     console.log(val)
 })
